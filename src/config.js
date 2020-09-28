502580a659552aee89c2a746c54281ff9bd646d6
omarlin <omarlinneftali1997@gmail.com>
2020-09-27 20:13:41 -0400

    store time landing version con login

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-const DBConnection = require("./DBconnection");
+const path = require("path");
+const DBConnection = require(path.join(__dirname, "DBConnection"));
 
 const PORT = process.env.PORT || 50000;
 
